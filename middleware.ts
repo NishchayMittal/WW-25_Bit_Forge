107c336786fabc935586e275217c4d249d409a66
NishchayMittal <mittal.nishchay1501@gmail.com>
2025-08-03 03:47:28 +0530

    middleware clerk fixing

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
-// Match only protected routes (like /dashboard or /api)
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
